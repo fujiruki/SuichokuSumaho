e39ed8a5c4b1599ebdbdcb92dc55c43b98a55c7b
fujiruki <fjt.suntree@gmail.com>
2015-05-06 09:49:54 +0900

    canvasに線を描画できるようにした
    canvasに描く準備はできた。
    ジャイロセンサが取得できているか今から確認する

---
+++ main.js
@@ -0,0 +1,44 @@
+var canvas;
+var alpha, beta, gamma;
+
+window.onload = function() {
+    init_res = init();
+    if (!init_res) {
+        // TODO:canvasが使えないよってメッセージを出したい
+    }
+    draw();
+};
+
+function init() {
+    // canvasを用意
+    canvas = document.getElementById("canvas1");
+    if ( ! canvas || ! canvas.getContext ) {
+        return false;
+    }
+
+    // ジャイロセンサ表示部を用意
+    alpha = document.getElementById("alpha");
+    beta = document.getElementById("beta");
+    gamma = document.getElementById("gamma");
+
+    return true;
+}
+function draw() {
+    var ctx = canvas.getContext('2d');
+
+    // 四角形を描く
+    ctx.beginPath();
+    ctx.moveTo(20, 20);
+    ctx.lineTo(120, 20);
+    ctx.stroke();
+}
+
+function beginSensing() {
+    window.addEventListener("deviceorientation", function(event){
+        alpha.textContent = event.alpha;
+        beta.textContent = event.beta;
+        gamma.textContent = event.gamma;
+
+        
+    }
+}
